889fbf084978702a043e36876674172dd342ebf2
CookieWookie <matej.hanajik@h-soft.sk>
2014-02-24 09:49:54 +0100

    pridany import parser interface

---
+++ Academic Information System/Repositories/IImportParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AiS.Repositories
+{
+    public interface IImportParser<T>
+    {
+        IEnumerable<T> Parse();
+    }
+}

0ed066d1aba6ec3ddd464f46bee767fcfd6db00e
CookieWookie <matej.hanajik@h-soft.sk>
2014-02-24 15:08:41 +0100

    projekt zmeneny na wpf app

---
+++ Academic Information System/ViewModels/TestMainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AiS.ViewModels
+{
+    class TestMainWindowViewModel : IMainWindowViewModel
+    {
+        public string Property1 { get; set; }
+
+        public TestMainWindowViewModel()
+        {
+            Property1 = "This is default value.";
+        }
+
+        public void OpenImportView()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenSaveView()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Academic Information System/ViewModels/TestSaveWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AiS.ViewModels
+{
+    public class TestSaveWindowViewModel
+    {
+        public string Property1 { get; set; }
+
+        public TestSaveWindowViewModel()
+        {
+            Property1 = "This is save window base value.";
+        }
+    }
+}

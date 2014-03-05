47d55710d12d6ad380719794be90c2f71db5642b
chipak96 <peter.gaba2@st.fm.uniba.sk>
2014-03-05 14:42:46 +0100

    subjects

--- Academic Information System/Repositories/Import/SubjectImportManager.cs
+++ Academic Information System/Repositories/Import/SubjectImportManager.cs
@@ -6,12 +6,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using AiS.Models;
-
+using System.IO;
 namespace AiS.Repositories.Import
 {
     public class SubjectImportManager : IImportManager<Subject>
     {
         private readonly IRepository<Subject> repository;
+        List<Subject> subjects = new List<Subject>();
 
         public IRepository<Subject> Repository
         {
@@ -23,15 +24,30 @@ namespace AiS.Repositories.Import
             repository.ThrowIfNull("repository");
             this.repository = repository;
         }
-        
+
         public void ParseFile(string file)
         {
-            throw new NotImplementedException();
+            using (StreamReader sr = new StreamReader(file))
+            {
+                string riadok = sr.ReadLine();
+                while (riadok != null)
+                {
+                    string[] delenyRiadok = riadok.Split(';');
+                    Subject sub = new Subject();
+                    sub.ID = delenyRiadok[0];
+                    sub.Name = delenyRiadok[1];
+                    sub.Semester = Convert.ToInt32(delenyRiadok[3]);
+                    if (!subjects.Contains(sub))
+                        subjects.Add(sub);
+                }
+            }
         }
 
         public void Save()
         {
-            throw new NotImplementedException();
+            Repository.Save(subjects.ToArray());
+            subjects.Clear();
+
         }
     }
 }

48f794e96727e6746c853888fbe50342730414d2
chipak96 <peter.gaba2@st.fm.uniba.sk>
2014-04-14 14:31:24 +0200

    asi to je na picku velice moc ale pozri... lengthofstudyvalidation..

--- Academic Information System/Views/Validators/LengthOfStudyValidationRule.cs
+++ Academic Information System/Views/Validators/LengthOfStudyValidationRule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Controls;
+using System.Globalization;
 
 namespace AiS.Views.Validators
 {
@@ -14,7 +15,20 @@ namespace AiS.Views.Validators
 
         public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
         {
-            throw new NotImplementedException();
+            DateTime dt;
+            bool b = DateTime.TryParseExact(value as string, new[] { "Rok" }, cultureInfo, DateTimeStyles.None, out dt);
+            if (value == null || !b)
+            {
+                return new ValidationResult(false, "Hodnota nie je platná.");
+            }
+            TimeSpan t = dt.TimeOfDay;
+            TimeSpan low = new TimeSpan(3);
+            TimeSpan up = new TimeSpan(6);
+            if (t < low || up < t)
+            {
+                return new ValidationResult(false, "Dĺžka štúdia musí byť od 3 rokov až po 6 rokov.");
+            }
+            return new ValidationResult(true, "");
         }
     }
 }

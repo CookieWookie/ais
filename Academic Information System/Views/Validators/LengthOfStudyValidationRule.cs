affffcbc8f3d9f37a7045d07084b2ee5b442fe4f
chipak96 <peter.gaba2@st.fm.uniba.sk>
2014-04-15 10:52:25 +0200

    length of study validation hotovo

--- Academic Information System/Views/Validators/LengthOfStudyValidationRule.cs
+++ Academic Information System/Views/Validators/LengthOfStudyValidationRule.cs
@@ -13,26 +13,24 @@ namespace AiS.Views.Validators
         {
         }
 
-        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)// cervena ciarka :-(
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
         {
             string x = value as string;
             int number;
             bool a = Int32.TryParse(x, out number);
             if (a == null)
             {
-                return new ValidationResult(false, "Zadaná hodnota musí byť číslo .");// zmen co nam napise ked tak
+                return new ValidationResult(false, "Zadaná hodnota musí byť číslo.");
+            }
 
-            }
-            if (a == true && (number < 3 || number > 6))
+            if (number < 3 || number > 6)
             {
-                return new ValidationResult(false, "Zadaná hodnota musí byť číslo>=3 alebo <=6 .");// zmen to co nam napise ked tak
- 
+                return new ValidationResult(false, "Dĺžka štúdia m´ôže byť v rozsahu od 3-6 rokov.");
             }
-            if (a == true && (number >= 3 || number <= 6))//ked tak podmienka do pice
-            {
+            
                 return new ValidationResult(true, "");
 
-            }
+
 
         }
     }

2c8d0fbe13da37539cdd1fdbc2739f62135cfb4e
Peracnik <adam.polaaak@gmail.com>
2014-04-24 08:40:48 +0200

    lalala

--- Academic Information System/Views/Validators/ExamDateValidationRule.cs
+++ Academic Information System/Views/Validators/ExamDateValidationRule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Controls;
+using System.Globalization;
 
 namespace AiS.Views.Validators
 {
@@ -14,7 +15,21 @@ namespace AiS.Views.Validators
 
         public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
         {
-            throw new NotImplementedException();
+            string x = value as string;
+            DateTime date;
+            bool b = DateTime.TryParseExact(x, new[] { "dd.MM.yyyy", "d.M.yyyy" }, cultureInfo, DateTimeStyles.None, out date);
+            if (b == null || !b)
+            {
+                return new ValidationResult(false, "Zadaná hodnota nie je platná");
+            }
+            DateTime today = DateTime.Now.AddDays;
+
+            if (date >= today)
+            {
+                return new ValidationResult(false, "Na skusku sa da prihlasit najskor 3 dni pred jej začiatkom");
+            }
+
+            return new ValidationResult(true, "");
         }
     }
 }

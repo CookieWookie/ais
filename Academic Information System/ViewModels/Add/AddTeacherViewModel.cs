e03fdee1d48c1efdc15f3a49cdb9e37e5824730c
Peracnik <adam.polaaak@gmail.com>
2014-04-24 08:29:33 +0200

    addteacher

--- Academic Information System/ViewModels/Add/AddTeacherViewModel.cs
+++ Academic Information System/ViewModels/Add/AddTeacherViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using AiS.Repositories;
 using AiS.Models;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
 
 namespace AiS.ViewModels
 {
@@ -17,6 +18,80 @@ namespace AiS.ViewModels
         private readonly ICommand addSubjectCommand;
         private readonly ICommand removeSubjectCommand;
         private List<Subject> subjects;
+
+        private string title;
+        private string name;
+        private string lastname;
+        private string titleSuffix;
+        private ObservableCollection<Subject> teaches;
+
+        public string Title
+        {
+            get { return this.title; }
+            set
+            {
+                if (value != this.title)
+                {
+                    this.title = value;
+                    this.OnPropertyChanged("Title");
+                }
+            }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (value != this.name)
+                {
+                    this.name = value;
+                    this.OnPropertyChanged("Name");
+                }
+            }
+        }
+        public string Lastname
+        {
+            get { return this.lastname; }
+            set
+            {
+                if (value != this.lastname)
+                {
+                    this.lastname = value;
+                    this.OnPropertyChanged("Lastname");
+                }
+            }
+        }
+        public string TitleSuffix
+        {
+            get { return this.titleSuffix; }
+            set
+            {
+                if (value != this.titleSuffix)
+                {
+                    this.titleSuffix = value;
+                    this.OnPropertyChanged("TitleSuffix");
+                }
+            }
+        }
+        public IList<Subject> Teaches
+        {
+            get
+            {
+                if (teaches == null)
+                {
+                    SetCollection(null);
+                }
+                return teaches;
+            }
+            set
+            {
+                if (value != this.teaches)
+                {
+                    this.SetCollection(value);
+                    this.OnPropertyChanged("Teaches");
+                }
+            }
+        }
 
         public override string WindowName
         {
@@ -59,12 +134,27 @@ namespace AiS.ViewModels
 
         public override void ResetToDefault()
         {
-            throw new NotImplementedException();
-        }
+            this.Title = this.original.Title;
+            this.Name = this.original.Name;
+            this.Lastname = this.original.Lastname;
+            this.TitleSuffix = this.original.TitleSuffix;
 
         public override void Save()
         {
-            throw new NotImplementedException();
+            Teacher teacher = new Teacher();
+            teacher.ID = this.original.ID;
+            teacher.Name = this.Name;
+            teacher.Title = this.Title;
+            teacher.Lastname = this.Lastname;
+            teacher.TitleSuffix = this.TitleSuffix;
+            repository.Save(teacher);
+            this.Close();
+        }
+        
+        private void SetCollection(IList<Subject> value)
+        {
+            this.teaches = value == null ? new ObservableCollection<Subject>() : new ObservableCollection<Subject>(value);
+            this.teaches.CollectionChanged += (sender, e) => this.OnPropertyChanged("Teaches");
         }
     }
 }
